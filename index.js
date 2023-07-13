09da42d746adc8f3f7507ff0763183b707f69238
asnat <asnatkemuma@gmail.com>
2023-07-13 23:02:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+ var customerName='bob'
+ 
+ function upperCaseCustomerName(){
+ return customerName= customerName.toUpperCase()
+ 
+ }
+ function setBestCustomer() {
+    bestCustomer = 'not bob';//undefined
+  }
+
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'}
+
+    const leastFavoriteCustomer = 'initial value';
+ function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This will throw an error
+}
